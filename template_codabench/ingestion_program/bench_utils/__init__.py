0ac9e230800c288aaa73b51c2f7d9d89d227220b
alizeemesnard <alizee.mesnard@telecom-sudparis.eu>
2026-03-06 17:06:31 +0100

    delete initial template

--- template_codabench/ingestion_program/bench_utils/__init__.py
+++
@@ -1,7 +0,0 @@
-# A module for the benchmark, to provide utilities usable in both the
-# ingestion and the submission programs.
-#
-# Note that the import cannot be done at the top level of
-# the ingestion program
-
-N_SAMPLES = 1000
